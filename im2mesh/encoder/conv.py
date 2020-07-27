fix bugs about batch normalization

im2mesh/encoder/conv.py
@@ -31,9 +31,10 @@ class ConvEncoder(tf.keras.Model):
         net = self.conv2(self.actvn(net))
         net = self.conv3(self.actvn(net))
         net = self.conv4(self.actvn(net))
-        net = net.view(batch_size, 512, -1).mean(2)
+        # net = net.view(batch_size, 512, -1).mean(2)
+        # net = tf.reduce_mean(tf.reshape(net, [batch_size, 512, -1]), axis=-1)
         net = tf.reshape(net, [batch_size, 512, -1])
-        net = tf.math.reduce_mean(net, 2)
+        net = tf.math.reduce_mean(net, axis=-1)
 
         out = self.fc_out(self.actvn(net))
 
@@ -48,18 +49,23 @@ class BasicBlock(tf.keras.layers.Layer):
                                             kernel_size=(3, 3),
                                             strides=stride,
                                             padding="same")
-        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.bn1 = tf.keras.layers.BatchNormalization(
+            momentum=0.1, epsilon=1e-05)
+
         self.conv2 = tf.keras.layers.Conv2D(filters=filter_num,
                                             kernel_size=(3, 3),
                                             strides=1,
                                             padding="same")
-        self.bn2 = tf.keras.layers.BatchNormalization()
+        self.bn2 = tf.keras.layers.BatchNormalization(
+            momentum=0.1, epsilon=1e-05)
+
         if stride != 1:
             self.downsample = tf.keras.Sequential()
             self.downsample.add(tf.keras.layers.Conv2D(filters=filter_num,
                                                        kernel_size=(1, 1),
                                                        strides=stride))
-            self.downsample.add(tf.keras.layers.BatchNormalization())
+            self.downsample.add(tf.keras.layers.BatchNormalization(
+                momentum=0.1, epsilon=1e-05))
         else:
             self.downsample = lambda x: x
 
@@ -104,7 +110,9 @@ class Resnet18(tf.keras.Model):
                                             kernel_size=(7, 7),
                                             strides=2,
                                             padding="same")
-        self.bn1 = tf.keras.layers.BatchNormalization()
+        self.bn1 = tf.keras.layers.BatchNormalization(
+            momentum=0.1, epsilon=1e-05)
+
         self.pool1 = tf.keras.layers.MaxPool2D(pool_size=(3, 3),
                                                strides=2,
                                                padding="same")

im2mesh/layers.py
@@ -136,8 +136,11 @@ class ResnetBlockConv1d(tf.keras.Model):
         self.size_h = size_h
         self.size_out = size_out
         # Submodules
-        self.bn_0 = tf.keras.layers.BatchNormalization()
-        self.bn_1 = tf.keras.layers.BatchNormalization()
+
+        self.bn_0 = tf.keras.layers.BatchNormalization(
+            momentum=0.1, epsilon=1e-05)
+        self.bn_1 = tf.keras.layers.BatchNormalization(
+            momentum=0.1, epsilon=1e-05)
 
         self.fc_0 = tf.keras.layers.Conv1D(size_h, 1)
         self.fc_1 = tf.keras.layers.Conv1D(size_out,
@@ -217,9 +220,10 @@ class CBatchNorm1d(tf.keras.Model):
         self.conv_gamma = tf.keras.layers.Conv1D(
             f_dim, 1, kernel_initializer='zeros', bias_initializer='ones')
         self.conv_beta = tf.keras.layers.Conv1D(
-            f_dim, 1,  kernel_initializer='zeros', bias_initializer='zeros')
+            f_dim, 1, kernel_initializer='zeros', bias_initializer='zeros')
         if norm_method == 'batch_norm':
-            self.bn = tf.keras.layers.BatchNormalization(trainable=False)
+            self.bn = tf.keras.layers.BatchNormalization(
+                trainable=False, momentum=0.1, epsilon=1e-05)
         elif norm_method == 'instance_norm':
             '''
             tfa.layers.InstanceNormalization doesn't have 'trainable' arguments
@@ -273,7 +277,8 @@ class CBatchNorm1d_legacy(tf.keras.Model):
         self.fc_beta = tf.keras.layers.Dense(
             f_dim, kernel_initializer='zeros', bias_initializer='zeros')
         if norm_method == 'batch_norm':
-            self.bn = tf.keras.layers.BatchNormalization(trainable=False)
+            self.bn = tf.keras.layers.BatchNormalization(
+                trainable=False, momentum=0.1, epsilon=1e-05)
         elif norm_method == 'instance_norm':
             '''
             tfa.layers.InstanceNormalization doesn't have 'trainable' arguments

im2mesh/onet/models/decoder.py
@@ -295,7 +295,8 @@ class DecoderBatchNorm(tf.keras.Model):
         self.block3 = ResnetBlockConv1d(hidden_size)
         self.block4 = ResnetBlockConv1d(hidden_size)
 
-        self.bn = tf.keras.layers.BatchNormalization(hidden_size)
+        self.bn = tf.keras.layers.BatchNormalization(
+            momentum=0.1, epsilon=1e-05)
 
         self.fc_out = tf.keras.Conv1D(1, 1)
 