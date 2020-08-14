c32cdaa3621afdb15a9a8d86fb6638a32165d12e
dkasuga <benjamin.at.mac@gmail.com>
2020-08-14 22:49:05 +0900

    fomrat coding style

--- train.py
+++ train.py
@@ -1,11 +1,26 @@
-import matplotlib
+# Copyright 2020 The TensorFlow Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
 import datetime
 import time
-import argparse
 import os
 import numpy as np
 import tensorflow as tf
-from im2mesh import config, data
+import matplotlib
+
+from im2mesh import config
 from im2mesh.checkpoints import CheckpointIO
 
 # from tensorboardX import SummaryWriter
@@ -35,22 +50,23 @@ exit_after = args.exit_after
 
 model_selection_metric = cfg['training']['model_selection_metric']
 if cfg['training']['model_selection_mode'] == 'maximize':
-    model_selection_sign = 1
+  model_selection_sign = 1
 elif cfg['training']['model_selection_mode'] == 'minimize':
-    model_selection_sign = -1
+  model_selection_sign = -1
 else:
-    raise ValueError('model_selection_mode must be '
-                     'either maximize or minimize.')
+  raise ValueError('model_selection_mode must be '
+                   'either maximize or minimize.')
 
 # Output directory
 if not os.path.exists(out_dir):
-    os.makedirs(out_dir)
+  os.makedirs(out_dir)
 
 # Dataset
 # specify path
 
 train_dataset = config.get_dataset(
-    'train', cfg, batch_size=batch_size, shuffle=True, repeat_count=1, epoch=100)
+    'train', cfg, batch_size=batch_size,
+    shuffle=True, repeat_count=1, epoch=100)
 val_dataset = config.get_dataset(
     'val', cfg, batch_size=10, shuffle=False, repeat_count=1, epoch=1)
 vis_dataset = config.get_dataset(
@@ -73,9 +89,9 @@ optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4, epsilon=1e-08)
 checkpoint_io = CheckpointIO(model, optimizer, model_selection_sign, out_dir)
 
 try:
-    checkpoint_io.load()
+  checkpoint_io.load()
 except FileExistsError:
-    print("start from scratch")
+  print("start from scratch")
 
 epoch_it = checkpoint_io.ckpt.epoch_it
 it = checkpoint_io.ckpt.it
@@ -92,13 +108,13 @@ trainer = config.get_trainer(model, optimizer, cfg)
 # Hack because of previous bug in code
 # TODO: remove, because shouldn't be necessary
 if metric_val_best == np.inf or metric_val_best == -np.inf:
-    metric_val_best = -model_selection_sign * np.inf
+  metric_val_best = -model_selection_sign * np.inf
 
 # TODO: remove this switch
 # metric_val_best = -model_selection_sign * np.inf
 
-print('Current best validation metric (%s): %.8f'
-      % (model_selection_metric, metric_val_best))
+print('Current best validation metric (%s): %.8f' %
+      (model_selection_metric, metric_val_best))
 
 # TODO: reintroduce or remove scheduler?
 # scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=4000,
@@ -119,58 +135,58 @@ train_summary_writer = tf.summary.create_file_writer(train_log_dir)
 test_summary_writer = tf.summary.create_file_writer(test_log_dir)
 
 while True:
-    # epoch_it += 1
-    epoch_it.assign_add(1)
-#     scheduler.step()
+  # epoch_it += 1
+  epoch_it.assign_add(1)
+  # scheduler.step()
 
-    for batch in train_loader:
-        # it += 1
-        it.assign_add(1)
-        loss = trainer.train_step(batch)
-        # logger.add_scalar('train/loss', loss, it)
+  for batch in train_loader:
+    # it += 1
+    it.assign_add(1)
+    loss = trainer.train_step(batch)
+    # logger.add_scalar('train/loss', loss, it)
 
-        # Print output
-        if print_every > 0 and (it % print_every) == 0:
-            print('[Epoch %02d] it=%03d, loss=%.4f' % (epoch_it, it, loss))
-            with train_summary_writer.as_default():
-                tf.summary.scalar('loss', loss, step=it)
+    # Print output
+    if print_every > 0 and (it % print_every) == 0:
+      print('[Epoch %02d] it=%03d, loss=%.4f' % (epoch_it, it, loss))
+      with train_summary_writer.as_default():
+        tf.summary.scalar('loss', loss, step=it)
 
-        # Visualize output
-        # if visualize_every > 0 and (it % visualize_every) == 0:
-        #     print('Visualizing')
-        #     trainer.visualize(data_vis)
+    # Visualize output
+    # if visualize_every > 0 and (it % visualize_every) == 0:
+    #     print('Visualizing')
+    #     trainer.visualize(data_vis)
 
-        # Save checkpoint
-        if (checkpoint_every > 0 and (it % checkpoint_every) == 0):
-            print('Saving checkpoint')
-            checkpoint_io.save('model.ckpt', epoch_it=epoch_it, it=it,
-                               loss_val_best=metric_val_best)
-        # Backup if necessary
-        if (backup_every > 0 and (it % backup_every) == 0):
-            print('Backup checkpoint')
-            checkpoint_io.save('model_%d.ckpt' % it, epoch_it=epoch_it, it=it,
-                               loss_val_best=metric_val_best)
-        # Run validation
-        if validate_every > 0 and (it % validate_every) == 0:
-            print("evaluate")
-            eval_dict = trainer.evaluate(val_loader)
-            print("eval_dict")
-            metric_val = eval_dict[model_selection_metric]
-            print('validation metric (%s): %.4f'
-                  % (model_selection_metric, metric_val))
+    # Save checkpoint
+    if (checkpoint_every > 0 and (it % checkpoint_every) == 0):
+      print('Saving checkpoint')
+      checkpoint_io.save('model.ckpt', epoch_it=epoch_it, it=it,
+                         loss_val_best=metric_val_best)
+    # Backup if necessary
+    if (backup_every > 0 and (it % backup_every) == 0):
+      print('Backup checkpoint')
+      checkpoint_io.save('model_%d.ckpt' % it, epoch_it=epoch_it, it=it,
+                         loss_val_best=metric_val_best)
+    # Run validation
+    if validate_every > 0 and (it % validate_every) == 0:
+      print("evaluate")
+      eval_dict = trainer.evaluate(val_loader)
+      print("eval_dict")
+      metric_val = eval_dict[model_selection_metric]
+      print('validation metric (%s): %.4f'
+            % (model_selection_metric, metric_val))
 
-            # for k, v in eval_dict.items():
-            # logger.add_scalar('val/%s' % k, v, it)
+      # for k, v in eval_dict.items():
+      # logger.add_scalar('val/%s' % k, v, it)
 
-            if model_selection_sign * (metric_val - metric_val_best) > 0:
-                metric_val_best = metric_val
-                print('New best model (loss %.4f)' % metric_val_best)
-                checkpoint_io.save('model_best.ckpt', epoch_it=epoch_it, it=it,
-                                   loss_val_best=metric_val_best)
+      if model_selection_sign * (metric_val - metric_val_best) > 0:
+        metric_val_best = metric_val
+        print('New best model (loss %.4f)' % metric_val_best)
+        checkpoint_io.save('model_best.ckpt', epoch_it=epoch_it, it=it,
+                           loss_val_best=metric_val_best)
 
-        # Exit if necessary
-        if exit_after > 0 and (time.time() - t0) >= exit_after:
-            print('Time limit reached. Exiting.')
-            checkpoint_io.save('model.ckpt', epoch_it=epoch_it, it=it,
-                               loss_val_best=metric_val_best)
-            exit(3)
+    # Exit if necessary
+    if exit_after > 0 and (time.time() - t0) >= exit_after:
+      print('Time limit reached. Exiting.')
+      checkpoint_io.save('model.ckpt', epoch_it=epoch_it, it=it,
+                         loss_val_best=metric_val_best)
+      exit(3)
